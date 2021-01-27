b798db1275a4793e45486e59a083e9ab590c4385
rcode1981 <78109181+rcode1981@users.noreply.github.com>
2021-01-28 04:37:24 +0800

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,15 @@
+<?php
+
+//BAHAN YANG PERLU DIINSTAL DI TERMUX
+//[1]. pkg install php
+//[2]. pkg install tesseract
+//[3]. pkg install imagemagick
+//Sampai Sini sudah Paham Kan 
+//Jangan Lupa Subscribe Chanel Penghasil Gratisa
+
+
+$ua = "Mozilla/5.0 (Linux; Android 9; ASUS_X00TD) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Mobile Safari/537.36";
+
+$PHPSESSID = "PHPSESSID=dfeai1nlp6utub9nifrl7567q0";
+
+$get_id = "get_id=127505";
